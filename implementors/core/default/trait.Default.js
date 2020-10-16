9b597adf5b3df319c0cf40c75450177df91d7048
MarioJim <MarioJim@users.noreply.github.com>
2020-10-16 04:14:25 +0000

    deploy: b836d51a530656f85211f1c0f605ffb139307a13

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["fuzzy_pdf"] = [{"text":"impl Default for Config","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
